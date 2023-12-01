bc955181e5c7731015528301fc2c169d51029d17
Caio Lins <clins7777777@gmail.com>
2023-12-01 17:14:20 +0900

    chore: birth by commit

---
+++ src/main/java/dev/clins/websocketsandbox/WebsocketSandboxApplication.java
@@ -0,0 +1,13 @@
+package dev.clins.websocketsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/clins/websocketsandbox/WebsocketSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.clins.websocketsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
